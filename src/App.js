1d076a4d3a0c21ee692c5e27bdbea9b489e037d8
dongyeon.kim <dongyeon.kim@lasee.io>
2023-03-29 08:48:02 +0900

    라이프사이클

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
 // function App() {
 //   const name = "리액트";
@@ -56,17 +57,44 @@ import IterationSample from "./IterationSample";
 //     );
 // };
 
+// class App extends Component {
+//     render() {
+//         return (
+//             // <div>
+//             //     <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+//             //     <button onClick={() => this.scrollBox.scrollToBottom()}>
+//             //         맨 밑으로
+//             //     </button>
+//             // </div>
+
+//             <IterationSample />
+//         );
+//     }
+// }
+
+//  라이프사이클
+//  랜덤색상지정
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+    state = {
+        color: "#000000",
+    };
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor(),
+        });
+    };
+
     render() {
         return (
-            // <div>
-            //     <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-            //     <button onClick={() => this.scrollBox.scrollToBottom()}>
-            //         맨 밑으로
-            //     </button>
-            // </div>
-
-            <IterationSample />
+            <div>
+                <button onClick={this.handleClick}>랜덤 색상</button>
+                <LifeCycleSample color={this.state.color} />
+            </div>
         );
     }
 }

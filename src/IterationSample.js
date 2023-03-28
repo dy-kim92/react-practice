6a2f97135539c6ece3abf18b301f4c9057e6b17d
dongyeon.kim <dongyeon.kim@lasee.io>
2023-03-28 16:37:46 +0900

    컴포넌트 반복 - 추가, 삭제

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const IterationSample = () => {
-    const names = ["눈사람", "얼음", "눈", "바람"];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-    return <ul>{nameList}</ul>;
+    const [names, setNames] = useState([
+        { id: 1, text: "눈사람" },
+        { id: 2, text: "얼음" },
+        { id: 3, text: "눈" },
+        { id: 4, text: "바람" },
+    ]);
+
+    const [inputText, setInputText] = useState("");
+    const [nextId, setNextId] = useState(5); //  새로운 항목을 추가할 때 사용할 id
+
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText,
+        });
+        setNextId(nextId + 1); //  id 증가
+        setNames(nextNames); //  name 값 업데이트
+        setInputText(""); //  초기화
+    };
+    const onRemove = (id) => {
+        const nextNames = names.filter((name) => name.id !== id);
+        setNames(nextNames);
+    };
+
+    const nameList = names.map((name) => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
+    );
 };
 
 export default IterationSample;
